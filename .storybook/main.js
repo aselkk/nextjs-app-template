450256406c8d01925e9d039bccb1b05161c92fb7
Asel Kk <aselkurmanbekk@gmail.com>
2023-02-05 21:52:05 +0600

    build: implement storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+export default {
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
+  staticDirs: ['../public'],
+};
